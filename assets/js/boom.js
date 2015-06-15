a358013d868924483e23a883acdc20c3026ba23a
Pavel Zaikin <zaikinpv@gmail.com>
2015-06-15 14:11:44 +0500

    append bootstrap template css
    Первый апдейт для конвертации шаблона под новый "дизайн"

---
+++ assets/js/boom.js
@@ -0,0 +1,119 @@
+$(window).resize(function(){
+				if ($(window).width() < 990) {
+				bl = $('.eee1').html();
+				$('.eee1').css('display','none');
+						$('.eee33').css('display','block');
+				$('.eee33').html(bl);
+				epg = 3;
+				}
+
+
+
+
+				else if ($(window).width() > 990) {
+					if (epg == 3) {
+				bl = $('.eee33').html();
+				$('.eee33').css('display','none');
+							$('.eee1').css('display','block');
+				$('.eee1').html(bl);
+					}
+
+
+
+				}
+				})
+
+
+
+
+				if ($(window).width() < 990) {
+				bl = $('.eee1').html();
+				$('.eee1').css('display','none');
+				$('.eee33').html(bl);
+				epg = 3;
+				}
+
+		
+
+	<script>
+$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 30) $('.eee1').addClass('fixed'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.eee1').removeClass('fixed');
+ });
+});
+
+
+
+
+$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 1) $('.vatb').addClass('asasa'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.vatb').removeClass('asasa');
+ });
+});
+
+$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 1) $('.menuu').addClass('navbar-fixed-top'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.menuu').removeClass('navbar-fixed-top');
+ });
+});
+
+		$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 1) $('.nav-tabs').addClass('gggg'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.nav-tabs').removeClass('gggg');
+ });
+});
+
+
+
+		$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 1) $('.navbar-brand').addClass('qwerty'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.navbar-brand').removeClass('qwerty');
+ });
+});
+
+
+			$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 1) $('.navbar-brand').addClass('gfgf'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.navbar-brand').removeClass('gfgf');
+ });
+});
+
+					$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 1) $('.avatar').addClass('asasa'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.avatar').removeClass('asasa');
+ });
+});
+
+
+							$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 1) $('.nav-tabs li a').addClass('atra'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.nav-tabs li a').removeClass('atra');
+ });
+});
+
+
+
+
+												$(function(){
+ $(window).scroll(function() {
+  var top = $(document).scrollTop();
+  if (top > 1) $('.nav-tabs li a').addClass('atra11'); //50 - это значение высоты прокрутки страницы для добавления класс
+  else $('.nav-tabs li a').removeClass('atra11');
+ });
+});
